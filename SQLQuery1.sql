2571c2913f70cd4afdd610e89688bb51ca76f98e
furkandmrz <furkanded00@gmail.com>
2025-12-29 18:48:39 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MarketStokTakip.Database
+{
+    class DbConnection
+    {
+        // LocalDB kullanıyorsan "Data Source=(localdb)\MSSQLLocalDB" yapmalısın.
+        // Veritabanı adının "MarketStokDb" olduğundan emin ol.
+        private static string yol = @"Server=(localdb)\MSSQLLocalDB;Database=MarketStokDb;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public static SqlConnection BaglantiGetir()
+        {
+            SqlConnection baglanti = new SqlConnection(yol);
+            
+            // Bağlantı kapalıysa açmak için küçük bir kontrol (opsiyonel ama iyidir)
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                // baglanti.Open(); // İstersen burada açabilirsin veya Form içinde açarsın.
+            }
+            
+            return baglanti;
+        }
+    }
+}
